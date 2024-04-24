507b78f38410db7407c70246b9be4837e09b754f
ivaperma <ivanpm89@gmail.com>
2024-04-24 12:24:26 +0200

    Funcionalidad completa. Botones editar y eliminar.

--- src/main/java/ipm/contactos/controlador/ContactoControlador.java
+++ src/main/java/ipm/contactos/controlador/ContactoControlador.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -29,14 +30,39 @@ public class ContactoControlador {
         modelo.put("contactos", contactos);
         return "index";
     }
+
     @GetMapping("/agregar")
-    public String mostrarAgregar(){
+    public String mostrarAgregar() {
         return "agregar";
     }
+
     @PostMapping("/agregar")
-    public String agregar(@ModelAttribute("contactoForma") Contacto contacto){
-        logger.info("Contacto a agregar: " +contacto);
+    public String agregar(@ModelAttribute("contactoForma") Contacto contacto) {
+        logger.info("Contacto a agregar: " + contacto);
         contactoServicio.guardarContacto(contacto);
         return "redirect:/"; //Redirigimos al controlador el path "/"
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarEditar(@PathVariable(value = "id") int idContacto, ModelMap modelo) {
+        Contacto contacto = contactoServicio.buscarContactoPorId(idContacto);
+        logger.info("Contacto a editar; " + contacto);
+        modelo.put("contacto", contacto);
+        return "editar";
+    }
+
+    @PostMapping("/editar")
+    public String editar(@ModelAttribute("contacto") Contacto contacto) {
+        logger.info("Contacto a guardar: " + contacto);
+        contactoServicio.guardarContacto(contacto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable(value = "id") int idContacto) {
+        Contacto contacto = new Contacto();
+        contacto.setIdContacto(idContacto);
+        contactoServicio.eliminarContacto(contacto);
+        return "redirecto:/";
     }
 }
